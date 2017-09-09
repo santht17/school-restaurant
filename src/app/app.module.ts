826db3e4a104ab32d7c44fd3caee51596c45befb
santht17 <santht17@hotmail.com>
2017-09-09 18:12:13 -0500

    porfin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import {CustomersModule} from './customers/customers.module';
 import { CurrentOptionComponent } from './current-option/current-option.component';
+import {OrdersModule} from './orders/orders.module';
 
+
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './customers/index/index.component';
+import { AddComponent as OrderAddComponent } from './orders/add/add.component';
+
+const appRoutes: Routes = [
+  { path: 'customers/index', component: IndexComponent },
+  { path: 'orders/add', component: OrderAddComponent },
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +24,12 @@ import { CurrentOptionComponent } from './current-option/current-option.componen
   ],
   imports: [
     BrowserModule,
-    CustomersModule
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
